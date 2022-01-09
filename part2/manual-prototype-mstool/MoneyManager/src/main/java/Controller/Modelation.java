18ce4cddb8f26b2c3aa7603a8648fc571e3f24b6
luis_vigario_1180990 <1180990@isep.ipp.pt>
2022-01-09 10:30:47 +0000

    #24 UserUI, TransactionUI, Modelation Code Generation

--- part2/manual-prototype-mstool/MoneyManager/src/main/java/Controller/Modelation.java
+++ part2/manual-prototype-mstool/MoneyManager/src/main/java/Controller/Modelation.java
@@ -2,59 +2,10 @@ package Controller;
 
 import Model.AccountGroup;
 import Model.User;
-
 import java.util.ArrayList;
 import java.util.List;
 
-/*import package Model.User;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-
-public class Controller.Modelation {
-    @Override
-    public String toString() {
-        return "Controller.Modelation{" +
-                "user=" + user.toString() +
-                ", accG=" + accG +
-                '}';
-    }
-
-    private User user;
-    private List<AccountGroup> accG = new ArrayList<>();
-
-    public Controller.Modelation(User user, List<AccountGroup> accG ) {
-        this.user = user;
-        this.accG = accG;
-    }
-
-    public Controller.Modelation() {
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<AccountGroup> getAccG() {
-        return accG;
-    }
-
-    public void setAccG(List<AccountGroup> accG) {
-        this.accG = accG;
-    }
-
-}
-*/
 public class Modelation {
-
 
     private User user;
     private List<AccountGroup> accG = new ArrayList<>();
@@ -62,9 +13,6 @@ public class Modelation {
     public Modelation(User user, List<AccountGroup> accG) {
         this.user = user;
         this.accG = accG;
-    }
-
-    public Modelation() {
     }
 
     public User getUser() {
